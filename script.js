1334e8fa7648a0cdad295e287df100f9833d1438
upar-rara <fulanaf09@gmail.com>
2022-06-08 22:46:20 +0700

    jam countdown fix

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ const seconds = document.getElementById("seconds");
 
 const currentYear = new Date().getFullYear();
 
-const newYearTime = new Date(`July 22 ${currentYear + 1} 00:00:00`);
+const WeddingTime = new Date(`July 22 ${currentYear + 1} 00:00:00`);
 
 // Update countdown time
 function updateCountdown() {
   const currentTime = new Date();
-  const diff = newYearTime - currentTime;
+  const diff = WeddingTime - currentTime;
 
   const d = Math.floor(diff / 1000 / 60 / 60 / 24);
   const h = Math.floor(diff / 1000 / 60 / 60) % 24;
   const m = Math.floor(diff / 1000 / 60) % 60;
-  const s = Math.floor(diff / 1000) & 60;
+  const s = Math.floor(diff / 1000) % 60;
 
   days.innerHTML = d;
   hours.innerHTML = h < 10 ? "0" + h : h;
